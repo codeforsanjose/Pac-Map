7c2a2d149975372623f1de1788d8a02489793c17
Tal Globus <talglobus@gmail.com>
2018-04-12 20:55:55 -0700

    Complete base server API functionality

--- server/routes/geo.js
+++ server/routes/geo.js
@@ -3,8 +3,17 @@ let jsonfile = require('jsonfile');
 let router = express.Router();
 let data = jsonfile.readFileSync('data/working_grouped_tazs.geojson');
 
-/* GET users listing. */
+// GET specific region
 router.get('/:region', function(req, res, next) {
+	if (data.features.hasOwnProperty(req.params.region)) {
+		res.json(data.features[req.params.region].geometry.coordinates);
+	} else {
+		res.json({});
+	}
+});
+
+// GET general area
+router.get('/', function(req, res, next) {
 	res.json(data);
 });
 
