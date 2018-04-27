8865ce69f6fe4e1ac1875304c7cf3f7b662b1d55
Tal Globus <talglobus@gmail.com>
2018-04-26 19:35:11 -0700

    Add demo  field

--- server/routes/geo.js
+++ server/routes/geo.js
@@ -3,10 +3,24 @@ let jsonfile = require('jsonfile');
 let router = express.Router();
 let data = jsonfile.readFileSync(__dirname + '/../data/working_grouped_tazs.geojson');
 
+router.get('/:region/:lastUpdatedTime', function(req, res, next) {
+	if (data.features.hasOwnProperty(req.params.region)) {
+		res.json({
+			data: data.features[req.params.region].geometry.coordinates,
+			lastUpdated: req.params.lastUpdatedTime
+		});
+	} else {
+		res.json({});
+	}
+});
+
 // GET specific region
 router.get('/:region', function(req, res, next) {
 	if (data.features.hasOwnProperty(req.params.region)) {
-		res.json(data.features[req.params.region].geometry.coordinates);
+		res.json({
+			data: data.features[req.params.region].geometry.coordinates,
+			lastUpdated: 0
+		});
 	} else {
 		res.json({});
 	}
